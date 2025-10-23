063f60fc5b3f80e02bda2161eb4209c44cac5aff
ArtemAzanov <azanov16@yandex.ru>
2025-10-23 16:07:37 +0300

    7 работа

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,18 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, b, c;
+    cout << "Введите a" << endl;
+    cin >> a;
+    cout << "Введите b" << endl;
+    cin >> b;
+    cout << "Введите c" << endl;
+    cin >> c;
+    double p = (a + b + c) / 2;
+    double S = sqrt(p * (p - a) * (p - b) * (p - c));
+    cout << "Результат " << S;
+
+}
