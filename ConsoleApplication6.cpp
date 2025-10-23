9006095aa2aa93860dee27bf0c434be8d87be262
ArtemAzanov <azanov16@yandex.ru>
2025-10-23 15:50:10 +0300

    8 работа

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,13 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double r;
+    double p = 3.14;
+    cout << "Введите радиус " << endl;
+    cin >> r;
+    double V = (4 / 3) * p * pow(r, 3);
+    cout << "Ответ " << V;
+}
