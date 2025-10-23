8ab5dee48ef57bdab75238805b651c7b9b5c93b4
ArtemAzanov <azanov16@yandex.ru>
2025-10-23 15:41:23 +0300

    7 работа

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,18 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double x1, x2, y1, y2;
+    cout << "Введите x1 ";
+    cin >> x1;
+    cout << "Введите x2 ";
+    cin >> x2;
+    cout << "Введите y1 ";
+    cin >> y1;
+    cout << "Введите y2 ";
+    cin >> y2;
+    double U = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    cout << "Результат " << U << endl;
+}
