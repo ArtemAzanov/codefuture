78ad04c1b221a47bb68320aac2479a9a5eb5d986
ArtemAzanov <azanov16@yandex.ru>
2025-10-23 15:25:42 +0300

    6 работа

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,15 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a = 5;
+    double h = 10;
+    cout << "Введите a ";
+    cin >> a;
+    cout << "Введите h ";
+    cin >> h;
+    double S = 0.5 * a * h;
+    cout << "Ответ: " << S;
+}
